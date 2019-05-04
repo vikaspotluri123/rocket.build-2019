111575e2d53efdf6dd811607ce2692bac37bdb08
Alex Borsbach <alexborsbach@Alexs-MacBook-Pro.local>
2019-05-04 13:32:59 -0500

    2nd upload of sign-up form

--- rocket.js
+++ rocket.js
@@ -1,4 +1,5 @@
-document.querySelector("#myForm").addEventListener("submit", function(e){
+document.querySelector("#sign-up").addEventListener("submit", function(e){
+    document.querySelector('input[name="username"]').value
     if(!isValid){
         e.preventDefault();    //stop form from submitting
     }
